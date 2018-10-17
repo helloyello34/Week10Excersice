2d6ba97add892e2bcd1e33d84b09fe19f14839db
helloyello34 <kristinn34@gmail.com>
2018-10-17 17:40:31 +0000

    SDFASETW

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // App.js
 
-const app = require("./src/server/api");
+const express = require("express");
+const app = express();
+const api = require("./src/server/api");
 
-const PORT = process.env.PORT || 3000;
+app.use("/api", api);
 
-app.listen(PORT, () => {
-  console.log("Server is running on port: " + PORT);
+app.get("*", (req, res) => {
+  res.send({ error: "No route defined" });
 });
+
+module.exports = app;
