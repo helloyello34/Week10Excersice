da247877ae14cecc7d0a325bc32a4a6048c1d3b5
helloyello34 <kristinn34@gmail.com>
2018-10-17 15:57:25 +0000

    BoilerPlate

---
+++ src/server/api.js
@@ -0,0 +1,23 @@
+// /scr/api.js
+
+const express = require("express");
+const app = express();
+const greeting = require('./greeting');
+
+app.get("/greeting/:name", (req, res) => {
+  // Fill in your code that:
+  // 1. set's the status to 200
+  // 2. and returns an object with the greeting
+  
+  // hint 'req.params.name'
+  // console.log(req);
+  res.status = 200;
+  if(req.params.name === "_") {
+	res.sendStatus(res.status);
+  } else {
+	var result = greeting(req.params.name);
+	res.send({ greeting: result });
+  }
+});
+
+module.exports = app;
