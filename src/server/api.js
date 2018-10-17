16a1f1cfedf0b8f6a0768a628015d2bdbd3081bb
helloyello34 <kristinn34@gmail.com>
2018-10-17 17:33:38 +0000

    heroku er fokking tregt

--- src/server/api.js
+++ src/server/api.js
@@ -1,10 +1,14 @@
 // src/api.js
 const express = require("express");
 const router = express.Router();
+const greeting = require("../logic/greeting");
+
+router.get("/greeting", (req, res) => {
+  res.status(404).send({error: "I don't know you" });
+});
 
 router.get("/greeting/:name", (req, res) => {
-  // Fix this code
-  res.send("Hi there");
+  res.status(404).send({ greeting: greeting(req.params.name) });
 });
 
 module.exports = router;
