061069a66d7f5cab8ec23960af26a3d701aa205b
helloyello34 <kristinn34@gmail.com>
2018-10-17 17:36:32 +0000

    !

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const app = require("./src/server/api");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Server is running on port: " + PORT);
