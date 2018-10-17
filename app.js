77f4df32e6d58d136a963af5a6229ad2d47bb87b
helloyello34 <kristinn34@gmail.com>
2018-10-17 17:43:32 +0000

    Er thad

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const api = require("./src/server/api");
 
-app.use("/api", api);
+app.use("./src/server/api", api);
 
 app.get("*", (req, res) => {
   res.send({ error: "No route defined" });
