cb16378b9a31b58b95230fb76198b386f6997e72
helloyello34 <kristinn34@gmail.com>
2018-10-17 17:56:09 +0000

    sidfjlasf

--- src/server/api.js
+++ src/server/api.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const greeting = require("../logic/greeting");
 
 router.get("/greeting", (req, res) => {
-  res.status(404).send({error: "I don't know you" });
+  res.status(503).send({error: "I don't know you" });
 });
 
 router.get("/greeting/:name", (req, res) => {
-  res.status(404).send({ greeting: greeting(req.params.name) });
+  res.status(503).send({ greeting: greeting(req.params.name) });
 });
 
 module.exports = router;
